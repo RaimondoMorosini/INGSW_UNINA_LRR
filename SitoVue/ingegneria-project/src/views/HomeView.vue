5a48dac2f367bbf96722258e8c798b1408b292e3
Cavaliere <s.lorenzo@studenti.unina.it>
2024-02-09 15:01:14 +0100

    setup della webapp vue

---
+++ SitoVue/ingegneria-project/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Welcome to the homepage of our awesome app!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ut perferendis aliquam, incidunt praesentium est fuga enim vero expedita neque error totam facere pariatur. Ullam sunt, qui perferendis beatae voluptatem culpa corrupti nihil assumenda veritatis doloribus saepe laboriosam. Ea doloremque dolores nostrum, dolor nam natus officia delectus sunt excepturi laboriosam dolore aperiam repellat voluptatum exercitationem vitae ullam possimus? Ipsa amet incidunt temporibus et harum dicta libero quo sapiente dolorum nihil perferendis modi deleniti quasi officia a repellat dolorem possimus quas, unde, voluptatem repudiandae ab. Deleniti aspernatur culpa quasi temporibus maiores? Sunt harum rem praesentium dolorum laudantium, architecto expedita delectus cupiditate.</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {  }
+}
+</script>
