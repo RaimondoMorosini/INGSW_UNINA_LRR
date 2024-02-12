b5aa63edfdc1e0159ef5ec273de0b3b2ab6b8d86
Cavaliere <s.lorenzo@studenti.unina.it>
2024-02-12 13:40:39 +0100

    404-not found page added

---
+++ SitoVue/ingegneria-project/src/views/NotFoundView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="not-found">
+    <h1>404 Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFoundView",
+};
+</script>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  margin-top: 100px;
+}
+
+h1 {
+  font-size: 36px;
+  color: #ff0000;
+}
+
+p {
+  font-size: 18px;
+  color: #333333;
+}
+</style>
