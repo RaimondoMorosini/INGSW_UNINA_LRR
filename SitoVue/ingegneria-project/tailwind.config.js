e4f97f18944e7f44fb5c1164833228167021b149
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-02-13 18:09:08 +0100

    colore primario aggiunto ai config di tailwind

--- SitoVue/ingegneria-project/tailwind.config.js
+++ SitoVue/ingegneria-project/tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primario: {
+          100: '#cd87f6',
+          200: '#c169f4',
+          300: '#9752c0',
+          400: '#904eb7',
+          500: '#8a4baf',
+          600: '#7c439d',
+          700: '#673883',
+          800: '#522d69',
+          900: '#3e214e',
+        },
+      },
+    },
   },
   plugins: [
     typography,
