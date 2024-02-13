1006d7871673809a386a9e1085f624dc8d048897
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-02-13 14:21:08 +0100

    aggiuntoi framework tailwind e modificato read me prototipatp base conponet da acnora finire

---
+++ SitoVue/ingegneria-project/src/components/menuItem.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    IconaSx: Object,
+    IconaDx: Object
+});
+</script>
+
+<template>
+    <button>
+        <component :is="IconaSx" />        
+        <slot></slot>
+        <component :is="IconaDx" />
+    </button>
+</template>
+
+<style scoped>
+button {
+    padding: 10px;
+    margin: 10px;
+    background-color: #2c3e50;
+    color: white;
+    stroke:white;
+    fill:white;
+    border: 0;
+    border-radius: 5px;
+}
+
+button:hover {
+    background-color: #42b983;
+}
+</style>
---
+++ SitoVue/ingegneria-project/src/views/TestRAi.vue
@@ -0,0 +1,44 @@
+<script setup>
+import icona from '../components/Icone/icona.vue';
+
+</script>
+
+<template>
+  <div class="test-rai">
+    <h1>TestRAi</h1>
+    <p>TestRAi</p>
+<icona class="prova" />
+  </div>
+    icona:
+
+    <menuItem :IconaSx="icona" testo2="Start"><input type="text"></menuItem>
+
+  <menuItem :IconaSx="icona" ><icona /></menuItem>
+
+
+  <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div class="md:flex">
+      <div class="md:flex-shrink-0">
+        <img class="h-48 w-full object-cover md:h-full md:w-48" src="../../../../Immagini prodotti/hobbista.jpg" alt="Descrizione Immagine">
+      </div>
+      <div class="p-8">
+        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Categoria</div>
+        <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Titolo dell'articolo</a>
+        <p class="mt-2 text-gray-500">Questo è un breve riepilogo dell'articolo. Tailwind CSS rende incredibilmente semplice stilizzare i tuoi componenti Vue.</p>
+      </div>
+    </div>
+  </div>
+
+
+
+</template>
+
+
+
+
+<style>
+.prova {
+    background-color: green;
+fill:orange !important;
+}
+</style>
