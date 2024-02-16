8ac89b964b9e1726d640fc4a9c12ef7fb96c6567
Cavaliere <s.lorenzo@studenti.unina.it>
2024-02-16 18:21:22 +0100

    Aggiunta di avatar di diverse dimensioni e forme

--- SitoVue/ingegneria-project/src/components/rai.vue
+++ SitoVue/ingegneria-project/src/components/rai.vue
@@ -1,7 +1,28 @@
 <template>
-  
-    <Avatar size="large" shape="square" name="Testi avatar" initials="TA" src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg" />
+  <div class="space-y-10">
+    <div class="flex item_center gap-8">
+      <Avatar shape="cerchio" size="small" name="test" initials="TS" src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"/>
+      <Avatar shape="cerchio" size="base" name="test" initials="TS" src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"/>
+      <Avatar shape="cerchio" size="large" name="test" initials="TS" src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"/>  
+    </div>
+    <div class="flex item_center gap-8">
+      <Avatar shape="cerchio" size="small" name="test" initials="TS" />
+      <Avatar shape="cerchio" size="base" name="test" initials="TS" />
+      <Avatar shape="cerchio" size="large" name="test" initials="TS"/>  
+    </div>
+    <div class="flex item_center gap-8">
+      <Avatar shape="square" size="small" name="test" initials="TS" src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"/>
+      <Avatar shape="square" size="base" name="test" initials="TS" src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"/>
+      <Avatar shape="square" size="large" name="test" initials="TS" src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"/>  
+    </div>
+    <div class="flex item_center gap-8">
+      <Avatar shape="square" size="small" name="test" initials="TS" />
+      <Avatar shape="square" size="base" name="test" initials="TS" />
+      <Avatar shape="square" size="large" name="test" initials="TS"/>  
+    </div>
 
+  </div>
+  
   </template>
   
   
