bc54350d681575e383fd7365ecf627abc52916ff
Cavaliere <s.lorenzo@studenti.unina.it>
2024-02-16 18:21:22 +0100

    Aggiunta di avatar di diverse dimensioni e forme

--- SitoVue/ingegneria-project/src/main.js
+++ SitoVue/ingegneria-project/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue';
 import router from './router';
 import './assets/css/tailwind.css';
 import './components/menuItem.vue';
-
+//da mettere nei file dove si vuole usare axios
+import axios from 'axios';
 
 
 // Crea l'istanza dell'app Vue e usa il router
