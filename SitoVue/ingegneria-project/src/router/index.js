7dc94a87cbe1b184300702c5c07f366b2c232250
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-02-20 18:23:21 +0100

    fix

--- SitoVue/ingegneria-project/src/router/index.js
+++ SitoVue/ingegneria-project/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-import TestRAi from '../views/TestRai.vue'
+import TestRai from '../views/TestRai.vue'
 import TestCategorie from '../views/TestCategorie.vue'
 import AuthLogin from '@/components/AuthLogin'
 
