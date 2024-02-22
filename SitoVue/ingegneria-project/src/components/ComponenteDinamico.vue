db21eb16d03290c25a59047db34a5465779c359c
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-02-22 18:01:15 +0100

    creati drowpdown menu per le categorie

---
+++ SitoVue/ingegneria-project/src/components/ComponenteDinamico.vue
@@ -0,0 +1,17 @@
+<template>
+    <component :is="as" v-bind="{ value }">
+      {{ value }}
+    </component>
+  </template>
+  
+  <script setup>
+  // Importa defineProps per definire le props nel convalue di <script setup>
+  defineProps({
+    as: {
+      type: [String, Object],
+      default: 'button'
+    },
+    value: String
+  });
+  </script>
+  
--- SitoVue/ingegneria-project/src/router/index.js
+++ SitoVue/ingegneria-project/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-import TestRAi from '../views/TestRAi.vue'
+import TestRai from '../views/TestRai.vue'
 import TestCategorie from '../views/TestCategorie.vue'
 import AuthLogin from '@/components/AuthLogin'
 
@@ -20,9 +20,9 @@ const routes = [
   },
 
   {
-    path: '/TestRAi',
-    name: 'TestRAi',
-    component : TestRAi
+    path: '/TestRai',
+    name: 'TestRai',
+    component : TestRai
   },
 
   {
