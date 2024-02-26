d783deb74be4abc43e604cdf045d428bb5b3865c
Cavaliere <s.lorenzo@studenti.unina.it>
2024-02-26 15:29:21 +0100

    creazione componente LogoSito

---
+++ SitoVue/ingegneria-project/src/components/LogoSito.vue
@@ -0,0 +1,13 @@
+<template>
+    <RouterLink to="/">
+    <div class="font-bold content-evenly w-40 bg-indigo-300 hover:bg-indigo-500 rounded mt-1 flex px-2 pl-3 py-1 pt-2 md:items-center md:px-0 md:pb-1">
+        <img src="../assets/Logo sito web [Optimized].svg" alt="">
+        <h1 class="text-2xl ">Deals24</h1>  
+        <h1 class="text-xs pt-4 ">RRL</h1>  
+    </div>
+    </RouterLink>
+</template>
+
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
