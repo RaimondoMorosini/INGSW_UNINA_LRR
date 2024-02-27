9fe6ba3b0708c41b1758b397974bcba8db7d5100
Cavaliere <s.lorenzo@studenti.unina.it>
2024-02-27 15:59:31 +0100

    Aggiornato componente Navbar.vue

--- SitoVue/ingegneria-project/src/components/Navbar.vue
+++ SitoVue/ingegneria-project/src/components/Navbar.vue
@@ -2,40 +2,38 @@
     <header class="sticky top-0 shadow-lg min-w-full z-50">
         <nav class="w-fill bg-indigo-300 md:flex flex-col sm:flex-row justify-between gap-5 py-1">
             
-            <Logo/>
+            <Logo class="min-w-1col"/>
+                   
+            <div>
+                <ul class="px-6 pt-2 md:flex md:items-center md:px-0 md:pb-1 md:static absolute bg-indigo-300 md:w-auto w-full top-14 duration-500 ease-in"
+                :class="[open ? 'left-0' : 'left-[-100%]']">
+                <li class="md:mx-4 md:my-0 my-6"> 
+                        <RouterLink :to="{name:'about'}">
+                        <p class="md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Ciao {{ profilo }}</p>
+                        </RouterLink> 
+                    </li>
+                    <li class="md:mx-4 md:my-0 my-6">    
+                        <RouterLink :to="{name:'about'}">
+                        <p class="md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Tue Aste</p>
+                        </RouterLink>
+                    </li>
+                    <li class="md:mx-4 md:my-0 my-6">
+                        <router-link :to="{ name : 'notifiche'}">
+                        <p class="md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Notifiche</p>
+                        </router-link>
+                    </li>
+                    <li class="md:mx-4 md:my-0 my-6">
+                        <InvetoryDropDown/>
+                    </li>
+                    <li class="md:mx-4 md:my-0 my-6">
+                    <p class="bg-indigo-400 md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Crea Asta</p>
+                    </li>
+                </ul>
+            </div>
             
-
-
             <span @click="MenuOpen()" class="absolute md:hidden right-6 top-1.5 hover:bg-indigo-500 rounded text-4xl">
                 <i :class="[open ? 'bi bi-x ' : 'bi bi-filter-left' ]"></i>    
             </span>
-
-            <div>
-            <ul class="px-6 pt-2 md:flex md:items-center md:px-0 md:pb-1 md:static absolute bg-indigo-300 md:w-auto w-full top-14 duration-500 ease-in"
-            :class="[open ? 'left-0' : 'left-[-100%]']">
-               <li class="md:mx-4 md:my-0 my-6"> 
-                    <RouterLink :to="{name:'about'}">
-                    <p class="md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Ciao {{ profilo }}</p>
-                    </RouterLink> 
-                </li>
-                <li class="md:mx-4 md:my-0 my-6">    
-                    <RouterLink :to="{name:'about'}">
-                    <p class="md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Tue Aste</p>
-                    </RouterLink>
-                </li>
-                <li class="md:mx-4 md:my-0 my-6">
-                    <router-link :to="{ name : 'notifiche'}">
-                    <p class="md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Notifiche</p>
-                    </router-link>
-                </li>
-                <li class="md:mx-4 md:my-0 my-6">
-                    <InvetoryDropDown/>
-                </li>
-                <li class="md:mx-4 md:my-0 my-6">
-                <p class="bg-indigo-400 md:mx-4 mt-1 block px-2 py-1 font-semibold rounded hover:bg-indigo-500">Crea Asta</p>
-                </li>
-            </ul>
-            </div>
         </nav>
     </header>
 </template>
@@ -45,7 +43,6 @@ import { RouterLink } from 'vue-router'
 import InvetoryDropDown from './InventoryDropDown.vue'
 import { ref } from 'vue'
 import Logo from "./LogoSito.vue";
-
 
 export default {
 
@@ -75,3 +72,19 @@ export default {
 }
 </script>
 
+
+<style>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
