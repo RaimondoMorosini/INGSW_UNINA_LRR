06b5875b4384cf9a7d697fd72202de48d88be163
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-03-01 18:35:42 +0100

    server spirng con rest  Ath0

--- ServerJavaBackend/ServerSpringJava/demo/src/main/java/com/example/demo/UserControllerRest.java
+++ ServerJavaBackend/ServerSpringJava/demo/src/main/java/com/example/demo/UserControllerRest.java
@@ -1,16 +1,21 @@
 package com.example.demo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserControllerRest {
 
     @GetMapping(path = "/user")
+    @PreAuthorize("hasAnyAuthority('SCOPE_readuser')")
     public String getuser(){
         return "Acces Granted : get User details";
     }
 
-    @PostMapping(path = "/user") 
+    @PostMapping(path = "/user")
+    @PreAuthorize("hasAnyAuthority('SCOPE_updateuser')")
     public String updateUser(){
         return "Acces Granted : update User details";
     }
