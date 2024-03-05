0bbbe0801a783e0825b1c2267ca72f185253bfd9
Cavaliere <s.lorenzo@studenti.unina.it>
2024-03-05 16:19:21 +0100

    Aggiornamenti al codice Vue

--- SitoVue/ingegneria-project/src/router/index.js
+++ SitoVue/ingegneria-project/src/router/index.js
@@ -1,110 +1,104 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import TestRai from '../views/TestRai.vue'
-import TestLorenzo from '../views/TestLorenzo.vue'
-import TestCategorie from '../views/TestCategorie.vue'
-import AuthLogin from '@/components/AuthLogin'
-import LoginView from '@/views/LoginView'
 
 const routes = [
 
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('@/views/HomeView.vue')
   },
 
   {
     path: '/about-us',
     name: 'about',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
 
   {
     path: '/loginTest',
     name: 'loginTest',
-    component: LoginView
+    component: () => import('@/views/LoginView.vue')
   },
   
   {
     path: '/profilo/astePersonali',
     name: 'astePersonali',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
 
   {
     path: '/categorie',
     name: 'categorie',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
 
   {
     path: '/categorie/elettronica',
     name: 'elettronica',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
 
   {
     path: '/categorie/perlacasa',
     name: 'perLaCasa',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
   {
     path: '/categorie/abbigliamento',
     name: 'abbigliamento',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
   {
     path: '/categorie/sports',
     name: 'sports',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
   {
     path: '/categorie/hobby',
     name: 'hobby',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
   {
     path: '/categorie/altro',
     name: 'altro',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
 
   {
     path: '/profilo/notifiche',
     name: 'notifiche',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
   {
     path: '/profilo',
     name: 'profilo',
-    component: AboutView
+    component: () => import('@/views/AboutView.vue')
   },
   {
     path: '/login',
     name: 'login',
-    component: AuthLogin
-  },
-
-  {
-    path: '/TestRai',
-    name: 'TestRai',
-    component : TestRai
+    component: () => import ('@/components/AuthLogin.vue')
   },
 
   // testing pages TODO: remove
 
   {
+    path: '/TestRai',
+    name: 'TestRai',
+    component :() => import ('@/views/TestRai.vue')
+  },
+
+
+  {
     path: '/TestLorenzo',
     name: 'TestLorenzo',
-    component : TestLorenzo
+    component :  () => import ('@/views/TestLorenzo.vue')
   },
 
   {
     path: '/testCategorie',
     name: 'testCategorie',
-    component : TestCategorie
+    component : () => import ('@/views/TestCategorie.vue')
   },
 
   // 404 page catch all
