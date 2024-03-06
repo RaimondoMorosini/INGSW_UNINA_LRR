b0411eb63644be222c8d9358bab33e46ebb367c3
Sperob98 <rbrna98@gmail.com>
2024-03-06 15:14:16 +0100

    test

--- SitoVue/ingegneria-project/src/views/TestCategorie.vue
+++ SitoVue/ingegneria-project/src/views/TestCategorie.vue
@@ -1,17 +1,21 @@
-<template>
-    <div></div>
-</template>
-  
-<script>
-import axios from 'axios'
-
-export default {
-    mounted() {
-        axios
-            .get('http://localhost:8080/testModel/api')
-            .then((response) => {
-                console.log(response.data)
-            })
-    }
+<script setup>
+import { decodeCredential,googleLogout,googleTokenLogin } from 'vue3-google-login'
+const callback = (response) => {
+  // decodeCredential will retrive the JWT payload from the credential
+  const userData = decodeCredential(response.credential)
+  console.log("Handle the userData", userData)
 }
+
+const getToken = () => {
+  googleTokenLogin().then((response) => {
+    console.log("token:", response)
+  })
+}
+
 </script>
+
+<template>
+  <GoogleLogin :callback="callback" />
+
+  <button @click="getToken">stampa token</button>
+</template>
