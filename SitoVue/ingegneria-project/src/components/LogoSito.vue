1a869439a50e4dac5019ed448fd7f836b931dc69
Cavaliere <s.lorenzo@studenti.unina.it>
2024-03-07 13:01:03 +0100

    Aggiornamenti di stile e correzioni di bug

--- SitoVue/ingegneria-project/src/components/LogoSito.vue
+++ SitoVue/ingegneria-project/src/components/LogoSito.vue
@@ -1,7 +1,8 @@
 <template>
     <RouterLink to="/">
-    <div class="font-bold content-evenly w-40 bg-inherit hover:bg-primario-500 rounded mt-1 flex px-2 pl-3 py-1 pt-2 md:items-center md:px-0 md:pb-1">
-        <logoIcon/>
+    <div class="font-bold content-evenly  bg-inherit hover:bg-primario-500 rounded mt-1 pr-5 flex px-3 py-2 md:items-center md:px-0 md:pb-1">
+        <!-- <logoIcon/> -->
+        <img src="../assets/Icon/LogoSVGSito.svg" alt="Logo" class="w-1col  h-1col" />
         <h1 class="text-2xl text-slate-900">Deals24</h1>  
         <h1 class="text-xs pt-[0.8rem] text-slate-900">RRL</h1>
      
