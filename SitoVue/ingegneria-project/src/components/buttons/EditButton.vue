c72f72362d5052c2f932b7531835dbfe200854eb
Cavaliere <s.lorenzo@studenti.unina.it>
2024-03-07 18:26:02 +0100

    Aggiornamento del profilo utente e della vista del profilo

---
+++ SitoVue/ingegneria-project/src/components/buttons/EditButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-slate-200 rounded px-2 py-1 flex flex-row space-x-1">
+        <button class="edit__login ">Modifica Informazioni </button>
+       <span> <i class="bi bi-pencil-square"></i> </span>
+    </div>
+
+</template>
+  
+<script setup>
+  
+ 
+</script>
--- SitoVue/ingegneria-project/src/views/ProfiloView.vue
+++ SitoVue/ingegneria-project/src/views/ProfiloView.vue
@@ -2,34 +2,29 @@
   
     <div  class="content-layout">
       <div class="content__body">
-        <div class="profile-grid flex">
-          <div class="min-w-3col h-[1200px] border-r-2  border-slate-300 flex-row">
+        < class="profile-grid flex">
+          <div class="min-w-3colflex-row">
             <ul class="border-b-2 border-slate-300 text-2xl">
               <li v-for="(pagina,index) in pagine" v-bind:key="index" class="hover:bg-primario-300 rounded py-2 px-2">
                 {{ pagina }}
               </li>
             </ul>
-            <p class="text-2xl">Settings</p>
+            <p class="text-2xl rounded py-2 px-2 hover:bg-primario-300">Settings</p>
           </div>
-
-          <div class="Componente-Inserito">
+          
+          <div class="Componente-Inserito w-full px-3 py-2 gap-2 flex flex-col">
             <InfoProfilo/>
             
           </div>
           
-        </div>
+        
       </div>
     </div>
 
 </template>
 
 <script setup>
-import { useAuth0 } from "@auth0/auth0-vue";
-import { onMounted } from "vue";
 import InfoProfilo from "@/components/SubComponenti/Profilo/InformazioniProfilo.vue";
 const pagine = ["Account", "Messaggi", "Tue Aste in Corso"];
-const { user } = useAuth0();
-
-const code = user ? JSON.stringify(user.value, null, 2) : "";
 
 </script>
