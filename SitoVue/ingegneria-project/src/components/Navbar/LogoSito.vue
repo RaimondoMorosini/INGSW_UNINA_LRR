706184f34e439f708a16f065dccf8cc611022b46
Cavaliere <s.lorenzo@studenti.unina.it>
2024-03-12 13:23:05 +0100

    Rimossi componenti e aggiornata navigazione

--- SitoVue/ingegneria-project/src/components/Navbar/LogoSito.vue
+++ SitoVue/ingegneria-project/src/components/Navbar/LogoSito.vue
@@ -2,7 +2,7 @@
     <RouterLink to="/">
     <div class="font-bold content-evenly  bg-inherit hover:bg-primario-300 rounded mt-1 flex px-5 md:items-center md:px-0 md:pb-2">
         
-        <img src="../../../assets/Icon/LogoSVGSito.svg" alt="Logo" class="w-1col  h-1col" />
+        <img src="../../assets/Icon/LogoSVGSito.svg" alt="Logo" class="w-1col  h-1col" />
         <div class="flex pt-[1.1rem] md:pt-0">
         <h1 class="text-2xl text-slate-900">Deals24</h1>  
         <h1 class="text-xs pt-[0.8rem] text-slate-900">RRL</h1>
