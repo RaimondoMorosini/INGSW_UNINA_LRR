2785caac9442a8d5e3200534364d531957e506a7
Cavaliere <s.lorenzo@studenti.unina.it>
2024-03-14 15:27:00 +0100

    Aggiornamenti dipendenze e corretta formattazione del componente SwipeCarosello

--- SitoVue/ingegneria-project/src/components/Navbar/ElementiNavBar.vue
+++ SitoVue/ingegneria-project/src/components/Navbar/ElementiNavBar.vue
@@ -1,72 +1,73 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
-  <div>
-    <ul
-      class="absolute top-[90px] w-full border-b-2 border-t-2 border-slate-300 bg-primario-100 px-6 py-6 text-xl duration-500 ease-in md:static md:flex md:w-auto md:items-center md:border-transparent md:bg-inherit md:px-0 md:pb-1"
-      :class="[open ? 'left-0 ' : 'left-[-100%] ']"
+    <div>
+        <ul
+            class="absolute top-[90px] w-full border-b-2 border-t-2 border-slate-300 bg-primario-100 px-6 py-6 text-xl duration-500 ease-in md:static md:flex md:w-auto md:items-center md:border-transparent md:bg-inherit md:px-0 md:pb-1"
+            :class="[open ? 'left-0 ' : 'left-[-100%] ']"
+        >
+            <template v-if="!isAuthenticated">
+                <li class="my-6 md:mx-4 md:my-0">
+                    <SignupButton
+                        class="mt-1 block w-[100%] rounded px-2 py-1 font-semibold hover:bg-primario-100 md:mx-4"
+                    />
+                </li>
+                <li class="my-6 md:mx-4 md:my-0">
+                    <LoginButton
+                        class="mt-1 block w-[100%] rounded px-2 py-1 font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
+                    />
+                </li>
+            </template>
+
+            <template v-if="isAuthenticated">
+                <li class="my-6 md:mx-4 md:my-0">
+                    <RouterLink :to="{ name: 'profilo' }">
+                        <p
+                            class="!sm:w-[100%] mt-1 block rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
+                        >
+                            Ciao {{ user.name }}
+                        </p>
+                    </RouterLink>
+                </li>
+                <li class="my-6 md:mx-4 md:my-0">
+                    <router-link to="/profilo/notifiche">
+                        <p
+                            class="mt-1 block rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
+                        >
+                            Notifiche
+                        </p>
+                    </router-link>
+                </li>
+                <li class="my-6 md:mx-4 md:my-0">
+                    <RouterLink :to="{ name: 'about' }">
+                        <p
+                            class="!sm:w-[100%] mt-1 block rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
+                        >
+                            Tue Aste
+                        </p>
+                    </RouterLink>
+                </li>
+                <li class="my-6 md:mx-4 md:my-0">
+                    <LogoutButton
+                        class="mt-1 block w-[100%] rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
+                    />
+                </li>
+                <li class="my-6 md:mx-4 md:my-0">
+                    <p
+                        class="!sm:w-[100%] mt-1 block rounded bg-primario-200 px-2 py-1 text-center font-semibold ring-2 ring-slate-300/80 hover:bg-primario-100 md:mx-4"
+                    >
+                        Crea Asta
+                    </p>
+                </li>
+            </template>
+        </ul>
+    </div>
+
+    <span
+        class="absolute right-6 top-5 rounded text-4xl hover:bg-primario-100 md:hidden"
+        @click="MenuOpen()"
     >
-      <template v-if="!isAuthenticated">
-        <li class="my-6 md:mx-4 md:my-0">
-          <SignupButton
-            class="mt-1 block w-[100%] rounded px-2 py-1 font-semibold hover:bg-primario-100 md:mx-4"
-          />
-        </li>
-        <li class="my-6 md:mx-4 md:my-0">
-          <LoginButton
-            class="mt-1 block w-[100%] rounded px-2 py-1 font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
-          />
-        </li>
-      </template>
-
-      <template v-if="isAuthenticated">
-        <li class="my-6 md:mx-4 md:my-0">
-          <RouterLink :to="{ name: 'profilo' }">
-            <p
-              class="!sm:w-[100%] mt-1 block rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
-            >
-              Ciao {{ user.name }}
-            </p>
-          </RouterLink>
-        </li>
-        <li class="my-6 md:mx-4 md:my-0">
-          <router-link to="/profilo/notifiche">
-            <p
-              class="mt-1 block rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
-            >
-              Notifiche
-            </p>
-          </router-link>
-        </li>
-        <li class="my-6 md:mx-4 md:my-0">
-          <RouterLink :to="{ name: 'about' }">
-            <p
-              class="!sm:w-[100%] mt-1 block rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
-            >
-              Tue Aste
-            </p>
-          </RouterLink>
-        </li>
-        <li class="my-6 md:mx-4 md:my-0">
-          <LogoutButton
-            class="mt-1 block w-[100%] rounded px-2 py-1 text-center font-semibold hover:bg-primario-100 hover:ring-2 hover:ring-slate-300/80 md:mx-4"
-          />
-        </li>
-        <li class="my-6 md:mx-4 md:my-0">
-          <p
-            class="!sm:w-[100%] mt-1 block rounded bg-primario-200 px-2 py-1 text-center font-semibold ring-2 ring-slate-300/80 hover:bg-primario-100 md:mx-4"
-          >
-            Crea Asta
-          </p>
-        </li>
-      </template>
-    </ul>
-  </div>
-
-  <span
-    @click="MenuOpen()"
-    class="absolute right-6 top-5 rounded text-4xl hover:bg-primario-100 md:hidden"
-  >
-    <i :class="[open ? 'bi bi-x ' : 'bi bi-filter-left']"></i>
-  </span>
+        <i :class="[open ? 'bi bi-x ' : 'bi bi-filter-left']"></i>
+    </span>
 </template>
 
 <script setup>
@@ -84,6 +85,4 @@ const open = ref(false)
 function MenuOpen() {
   open.value = !open.value
 }
-
-const profilo = ref('Utente')
 </script>
