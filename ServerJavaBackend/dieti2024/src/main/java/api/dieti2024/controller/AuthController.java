43bd5496babf3b85c7127edd93a1187c260ce10e
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-03-15 14:51:08 +0100

    eliminato vecchio server

---
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/AuthController.java
@@ -0,0 +1,29 @@
+package api.dieti2024.controller;
+
+import api.dieti2024.dto.CredenzialiUtenteDTO;
+import api.dieti2024.dto.ReqRes;
+import api.dieti2024.security.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/testhello")
+    public String testHello(){
+        return "Hello";
+    }
+    @PostMapping("/signup")
+    public String genTokenAfterSingUp(@RequestBody CredenzialiUtenteDTO signUpRequest){
+        return authService.registrazione(signUpRequest);
+    }
+    @PostMapping("/login")
+    public String GenTokenBylogin(@RequestBody CredenzialiUtenteDTO credenzialiInserite){
+        return (authService.login(credenzialiInserite));
+    }
+}
