957e6e79887ed7b148d9ac89d380a634857f60a8
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-03-18 18:08:42 +0100

    add modifica dati profilo
    put http://localhost:8080/utente/datiProfilo
    {
        "nome": "robertoooo",
        "cognome": "conomeee",
        "siti": null,
        "areaGeografica": "Napoli (NA)",
        "bio": null
    }
    
    Co-Authored-By: Roberto Spena <105874925+Sperob98@users.noreply.github.com>

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/UtenteController.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/UtenteController.java
@@ -2,27 +2,32 @@ package api.dieti2024.controller;
 
 import api.dieti2024.dto.DatiProfiloUtenteDTO;
 import api.dieti2024.exceptions.ApiException;
+import api.dieti2024.util.ControllerRestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/utente")
 public class UtenteController {
 
+    private final ControllerRestUtil controllerUtil = new ControllerRestUtil();
     @Autowired
     private api.dieti2024.service.utenteService utenteService;
     @GetMapping("/datiProfilo")
       public DatiProfiloUtenteDTO getDatiProfilo(@RequestParam String email){
         return utenteService.getDatiProfilo(email);
     }
-    @PutMapping("/datiProfilo/{email}")
-    public ResponseEntity updateDatiProfilo(String email, @RequestBody DatiProfiloUtenteDTO datiProfiloUtenteDTO){
+    @PutMapping("/datiProfilo")
+    public ResponseEntity updateDatiProfilo( @RequestBody DatiProfiloUtenteDTO datiProfiloUtenteDTO){
+
+        String email = controllerUtil.getEmailOfUtenteCorrente();
+
         try{
             utenteService.updateDatiProfilo(email,datiProfiloUtenteDTO);
-            return ResponseEntity.ok().build();
+            ResponseEntity resp = ResponseEntity.status(HttpStatus.OK).body("Dati aggiornati\n"+datiProfiloUtenteDTO);
+            return resp;
         }catch (ApiException e) {
             String message = e.getMessage();
             HttpStatus status = e.getStatus();
@@ -32,4 +37,5 @@ public class UtenteController {
         }
 
     }
+
 }
