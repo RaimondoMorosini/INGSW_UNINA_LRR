302ebc0bfda20a7d0bec5c8087ecf7d7e7d2106d
Cavaliere <s.lorenzo@studenti.unina.it>
2024-03-18 15:41:54 +0100

    Aggiornamento delle dipendenze e delle viste del profilo

--- SitoVue/ingegneria-project/src/views/TestCategorie.vue
+++ SitoVue/ingegneria-project/src/views/TestCategorie.vue
@@ -1,20 +1,3 @@
-<script setup>
-import { decodeCredential, googleLogout, googleTokenLogin } from 'vue3-google-login';
-const callback = (response) => {
-  // decodeCredential will retrive the JWT payload from the credential
-  const userData = decodeCredential(response.credential);
-  console.log('Handle the userData', userData);
-};
-
-const getToken = () => {
-  googleTokenLogin().then((response) => {
-    console.log('token:', response);
-  });
-};
-</script>
-
 <template>
-  <GoogleLogin :callback="callback" />
-
-  <button @click="getToken">stampa token</button>
+  <div>test</div>
 </template>
