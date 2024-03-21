50ada54a63bbd849b57c36a3df1b563b0cd55b76
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-03-21 15:11:07 +0100

    agigunti datiUtils
    salva i dati nella sessione

---
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/NotificheController.java
@@ -0,0 +1,25 @@
+package api.dieti2024.controller;
+
+import api.dieti2024.dto.NotificaDTO;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificheController {
+
+
+    @MessageMapping("/notifica")
+    @SendTo("/topic/notifica")
+    public NotificaDTO inviaNotifica(@Payload NotificaDTO notificaDTO){
+        return notificaDTO;
+    }
+
+    public NotificaDTO addUtente(@Payload NotificaDTO notificaDTO, SimpMessageHeaderAccessor headerAccessor){
+        //aggiungo l'utente alla sessione tramite l'email per poterlo identificare
+        headerAccessor.getSessionAttributes().put("email", notificaDTO.email());
+        return notificaDTO;
+    }
+}
--- SitoVue/ingegneria-project/src/views/TestCategorie.vue
+++ SitoVue/ingegneria-project/src/views/TestCategorie.vue
@@ -1,3 +1,37 @@
+<script setup>
+import { inserisciDato, checkEsisteDato ,eliminaDato} from '../scripts/DatiUtils';
+import { ref } from "vue";
+
+let datoEsiste = false;
+let messaggio = ref ("Il dato non esiste!"); 
+let nomeDato = 'dato2';
+let valoreDato = 'val1';
+const insDato = () => {
+  console.log('insDato');
+  inserisciDato(nomeDato, valoreDato);
+};
+
+const controllaDato = async () => {
+  datoEsiste = await checkEsisteDato(nomeDato);
+  console.log('datoEsiste', datoEsiste);
+  
+  if (datoEsiste) {
+    messaggio.value = 'Il dato esiste!';
+  } else {
+    messaggio.value = 'Il dato non esiste!';
+  }
+};
+
+
+</script>
 <template>
-  <div>test</div>
+  <div>
+    <button class="hover:bg-slate-400" @click="insDato">Inserisci Dato</button>
+    <br>
+    <button class="hover:bg-slate-400 "@click="controllaDato ">Controlla se il Dato Esiste</button>
+    <br>
+    <button class="hover:bg-slate-400 "@click="eliminaDato(nomeDato) ">elimina Dato</button>
+    <p >{{ messaggio }}</p>
+  </div>
 </template>
+
