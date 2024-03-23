f6b4503a8f752eea86a22475a3490642c161e778
Sperob98 <rbrna98@gmail.com>
2024-03-23 13:43:11 +0100

    Versione definitiva getCategorie Server
    Fissaggio della costruzione della gerarchia delle categorie

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/service/CategoriaService.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/service/CategoriaService.java
@@ -1,12 +1,73 @@
 package api.dieti2024.service;
 
+import api.dieti2024.dto.CategoriaDTO;
 import api.dieti2024.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CategoriaService {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    public List<CategoriaDTO> costruisciGerarchiaCategorie(){
+
+        List<CategoriaDTO> listaCategorieDaRestituire = new ArrayList<>();
+
+        List<String> categorieRadici =  categoriaRepository.getCategorieRadice();
+
+        List<String> figli;
+
+        /*
+            Per ogni categoria radice estraiamo la lista dei suoi figli
+
+         */
+        for(String categoriaRadice : categorieRadici){
+
+            CategoriaDTO categoriaRadiceDTO = new CategoriaDTO(categoriaRadice);
+
+            if( (categoriaRepository.getNumeroFigli(categoriaRadice)) > 0 ){
+
+                figli = categoriaRepository.getCategorieFiglie(categoriaRadice);
+
+                /*
+                    Per ogni figlio della categoria Radice viene fatto la stessa cosa ricorsivamente
+                 */
+                for(String figlio : figli){
+
+                    CategoriaDTO figlioDTO = new CategoriaDTO(figlio);
+
+                    costruisciGerarchiaRicorsivamente(figlioDTO);
+
+                    categoriaRadiceDTO.getChildren().add(figlioDTO);
+                }
+            }
+
+            listaCategorieDaRestituire.add(categoriaRadiceDTO);
+        }
+
+        return listaCategorieDaRestituire;
+    }
+
+    private void costruisciGerarchiaRicorsivamente(CategoriaDTO categoriaParametro){
+
+        if( (categoriaRepository.getNumeroFigli(categoriaParametro.getLabel())) > 0 ){
+
+            List<String> figli = categoriaRepository.getCategorieFiglie(categoriaParametro.getLabel());
+
+            for (String figlio : figli){
+
+                CategoriaDTO figlioDTO = new CategoriaDTO(figlio);
+
+                costruisciGerarchiaRicorsivamente(figlioDTO);
+
+                categoriaParametro.getChildren().add(figlioDTO);
+            }
+        }
+    }
+
 }
