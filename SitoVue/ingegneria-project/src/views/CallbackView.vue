f3b6ad197f70fa2ee65a4109b5765f1cec2b5a9e
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-04-15 13:17:09 +0200

    collegato front e back per il login

--- SitoVue/ingegneria-project/src/views/CallbackView.vue
+++ SitoVue/ingegneria-project/src/views/CallbackView.vue
@@ -19,17 +19,43 @@
 </template>
 
 <script setup>
+import { onUnmounted } from 'vue';
 import { useAuth0 } from '@auth0/auth0-vue';
 import Skeleton from 'primevue/skeleton';
+import { useRoute ,useRouter  } from 'vue-router';
+import { postRest } from '../scripts/RestUtils';
+import {inserisciDato} from '../scripts/DatiUtils';
 
-const {idTokenClaims,getAccessTokenSilently  } = useAuth0();
-onMounted(async () => {
+const {idTokenClaims,isAuthenticated,user } = useAuth0();
+const router = useRouter();
+const route = useRoute();
+onUnmounted(async () => {
   try {
-    console.log('Getting token');
-    console.log("idtoken",idTokenClaims);
-    console.log("token ", getAccessTokenSilently.toString());
+    if (!isAuthenticated.value) {
+      console.error('User is not authenticated'); 
+      router.push({name:'home' })
+    }
+    //chiamata post per ottenere il token dati richiesti: public record CredenzialiUtenteDTO(String email, String password,String metodoDiRegistrazione) {
+    const bodyData = {
+      email:  idTokenClaims.value.email,
+      password: idTokenClaims.value.__raw,
+      metodoDiRegistrazione: 'auth0',
+    };
+
+    const token = await postRest('auth/login', bodyData);
+    if (token) {
+      console.log('token ottenuto', token);
+      inserisciDato('token', token);
+    }else{
+      console.error('token non ottenuto');
+      router.push({name:'home' })
+    }
+
+    console.log("access token",JSON.stringify(idTokenClaims.value.__raw));  
   } catch (error) {
     console.error('errore getting token ',error);
+    router.push({name:'home' })
+
   }
 });
 </script>
