d59d6167718e4e10474db9e77d30c8e16309f9b2
Sperob98 <rbrna98@gmail.com>
2024-05-06 15:39:18 +0200

    Fix getAsta

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/ProdottoAstaController.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/ProdottoAstaController.java
@@ -28,10 +28,10 @@ public class ProdottoAstaController {
 
     @Autowired
     AstaFacadeService astaFacadeService;
-    @Autowired
-    ImageContainerUtil imageContainerUtil;
+    /*@Autowired
+    ImageContainerUtil imageContainerUtil;*/
 
-    @GetMapping("public/asta/getAllAste")
+    @PostMapping("public/asta/getAllAste")
     public List<InfoDatiAstaDTO> getAllAste(@RequestBody FiltroDto filtroDto){
         try {
             return astaService.getAllAstaProdotto(filtroDto);
@@ -57,7 +57,7 @@ public class ProdottoAstaController {
 
     }
 
-    @PostMapping("public/asta/uploadImage")
+    /*@PostMapping("public/asta/uploadImage")
     public String uploadImage(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             return "Errore: Nessun file selezionato";
@@ -70,8 +70,6 @@ public class ProdottoAstaController {
         } catch (Exception e) {
             return "Errore durante il caricamento dell'immagine: " + e.getMessage();
         }
-    }
-
-
+    }*/
 
 }
--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/dto/asta/ricerca/InfoDatiAstaDTO.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/dto/asta/ricerca/InfoDatiAstaDTO.java
@@ -12,26 +12,36 @@ public class InfoDatiAstaDTO {
     @Id
     @Column(name = "id")
     int idProdottoAsta;
+
     @Column(name = "base_asta")
     double baseAsta;
+
     @Column(name = "prezzo_attuale")
     double prezzoAttuale;
+
     @Column(name = "data_scadenza")
     long dataScadenza;
+
     @Column(name = "data_inizio")
     long dataInizio;
+
     @Column(name = "tipo")
     String tipoAsta;
+
     @Column(name = "utente_creatore")
     String emailUtenteCreatore;
+
     @Column(name = "nome_prodotto")
     String nome;
+
     @Column(name = "immagini")
     @ElementCollection
     @CollectionTable(name = "prodotto_immagini", joinColumns = @JoinColumn(name = "prodotto_id"))
     List<String> immagini;
+
     @Column(name = "descrizione")
     String descrizione;
+
     @Column(name = "categoria")
     String categoria;
 }
