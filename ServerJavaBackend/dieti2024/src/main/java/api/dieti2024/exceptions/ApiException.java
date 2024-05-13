119424129197abc155a13907d6cbb7e7fc39d553
Cavaliere <s.lorenzo@studenti.unina.it>
2024-05-13 12:49:48 +0200

    applicazione formatter automatico

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/exceptions/ApiException.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/exceptions/ApiException.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ApiException extends RuntimeException {
 
     private final HttpStatus status;
+
     public ApiException(String messaggio, HttpStatus httpStatus) {
         super(messaggio);
         this.status = httpStatus;
