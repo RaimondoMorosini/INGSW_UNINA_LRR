f7e9cba7e5d28195ac062ef52554577a1d4f219c
Cavaliere <s.lorenzo@studenti.unina.it>
2024-05-13 16:34:00 +0200

    chore: Remove unused formkit.config.js file and update formkit configuration in main.js

--- SitoVue/ingegneria-project/formkit.config.js
+++
@@ -1,8 +0,0 @@
-import { defineFormKitConfig } from '@formkit/vue'
-import { rootClasses } from './formkit.theme.mjs'
- 
-export default defineFormKitConfig({
-  config: {
-    rootClasses
-  }
-})
--- SitoVue/ingegneria-project/src/main.js
+++ SitoVue/ingegneria-project/src/main.js
@@ -15,14 +15,9 @@ import './assets/css/tailwind.css'; // tailwindcss
 import router from './router'; // router
 import SockJS from 'sockjs-client'; //modulo per socket
 import { Stomp } from '@stomp/stompjs'; //stomp canale 1 via per messaggi
-import { plugin, defaultConfig } from '@formkit/vue' //formkit
-import config from '../formkit.config' //formkit config
 
 // Crea l'istanza dell'app Vue e usa il router
 const app = createApp(App).use(router);
-
-//iniziolizzazione formkit
-app.use(plugin, defaultConfig(config));
 
 // inizializzazione axios
 axios.defaults.baseURL = 'localhost:8081/';
