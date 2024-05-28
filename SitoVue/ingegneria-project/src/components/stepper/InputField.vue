e690b8e9313c2ec3836728651c23c98a0bd9290f
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-05-28 13:15:07 +0200

    creato component vue per gestire le info extra

---
+++ SitoVue/ingegneria-project/src/components/stepper/InputField.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Dropdown from 'primevue/dropdown';
+import { defineProps, ref ,defineEmits} from 'vue';
+
+const props = defineProps({
+  label: String,
+  options: Array,
+  modelValue: String,
+});
+
+const selectedValue = ref(null);
+
+const emit = defineEmits(['update:valoreSelezionato','update:modelValue']);
+const handleChange = (newValue) => {
+  selectedValue.value = newValue.value;
+  emit('update:valoreSelezionato', newValue.value);
+  emit('update:modelValue', newValue.value);
+};
+</script>
+
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <br>
+    <Dropdown showClear  v-model="selectedValue" :options="options" placeholder="Seleziona" @change="handleChange" />
+    <br>
+    Valore selezionato: {{ selectedValue }}
+  </div>
+</template>
--- SitoVue/ingegneria-project/src/components/stepper/impostaFiltriCreaProdotto.vue
+++ SitoVue/ingegneria-project/src/components/stepper/impostaFiltriCreaProdotto.vue
@@ -1,31 +1,33 @@
 <template>
-    <form @submit.prevent="gestioneInvio">
-      <div class="mx-2 my-2 px-2 py-2 ring-2 ring-black">
-        <InputField v-for="caratteristica in caratteristicheRelativeAllaCategoria"
-                    :key="caratteristica.id"
-                    :label="caratteristica.label"
-                    :options="caratteristica.opzioniSelezionabili"
-                    v-model="arrayValoriSelezionati[caratteristica.id]" >
-                </InputField>
-                {{ arrayValoriSelezionati }}
-        <hr class="my-4">
-      </div>
+    <div>
+      <form @submit.prevent="gestioneInvio">
+      
+        <div class="mx-2 my-2 px-2 py-2 ring-2 ring-black">
+  <InputField v-for="caratteristica in caratteristicheRelativeAllaCategoria"
+              :key="caratteristica.id"
+              :label="caratteristica.label"
+              :options="caratteristica.opzioniSelezionabili"
+                v-model="arrayValoriSelezionati[caratteristica.id]"
+              >
+  </InputField>    
+  arrayValoriSelezionati {{ arrayValoriSelezionati }} <br>
+  <hr class="my-4">
+</div>
   
-      <div class="areaBottoni flex justify-around gap-5">
-        <button class="previous bottone w-[30%] px-5" @click="goToPreviousForm" type="button">
-          Precedente
-        </button>
-        <button class="bottone w-[30%] px-5" type="submit">Successivo</button>
-      </div>
-    </form>
+        <div class="areaBottoni flex justify-around gap-5">
+          <button class="previous bottone w-[30%] px-5" @click="goToPreviousForm" type="button">
+            Precedente
+          </button>
+          <button class="bottone w-[30%] px-5" type="submit">Successivo</button>
+        </div>
+      </form>
+    </div>
   </template>
   
   <script setup>
-  import Dropdown from 'primevue/dropdown';
   import { defineEmits, onMounted, ref } from 'vue';
   import { getCaratteristiche } from '../../service/carateristicheService.js';
-  import InputField from './inputFieldCaratteristiche.vue'; // Assicurati che il percorso sia corretto
-  
+  import InputField from './InputField.vue';
   const emit = defineEmits(['update:active']);
   
   const gestioneInvio = () => {
@@ -37,7 +39,8 @@
     emit('update:active', 0);
   };
   
-  const arrayValoriSelezionati = ref([]);
+  const arrayValoriSelezionati = ref({});
+  
   const caratteristicheRelativeAllaCategoria = ref([]);
   
   onMounted(() => {
@@ -47,6 +50,7 @@
       console.log(error);
     });
   });
+  
   </script>
   
   <style scoped>
