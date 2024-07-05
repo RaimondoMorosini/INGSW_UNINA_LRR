d3c2574aa98e27255c96acccf397568861d7582d
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-07-05 16:22:46 +0200

    Refactor: Aggiunto costruttore privato a TipoAsta
    È stato aggiunto un costruttore privato alla classe TipoAsta per prevenire la sua istanziazione, in quanto è una classe di utilità che contiene solo metodi statici

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/util/TipoAsta.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/util/TipoAsta.java
@@ -1,6 +1,10 @@
 package api.dieti2024.util;
 
 public class TipoAsta {
+    // Costruttore privato per impedire l'istanziazione
+    private TipoAsta() {
+        throw new IllegalStateException("Utility class");
+    }
     public static final String INGLESE = "asta_inglese";
 
     public static final String SILENZIOSA = "asta_silenziosa";
