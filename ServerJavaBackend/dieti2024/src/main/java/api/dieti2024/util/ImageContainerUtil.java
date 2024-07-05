df29046069ac8ab81b2b1654e61114fd568c551a
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-07-05 16:08:57 +0200

    Update ImageContainerUtil.java

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/util/ImageContainerUtil.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/util/ImageContainerUtil.java
@@ -4,7 +4,6 @@ package api.dieti2024.util;
 import api.dieti2024.exceptions.ApiException;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,10 +12,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageContainerUtil {
 
 
-    @Autowired
-    private    BlobContainerClient blobContainerClient;
+    private final BlobContainerClient blobContainerClient;
 
-     public void uploadImage(MultipartFile file,String nomePath) {
+    public ImageContainerUtil(BlobContainerClient blobContainerClient) {
+        this.blobContainerClient = blobContainerClient;
+    }
+
+    public void uploadImage(MultipartFile file,String nomePath) {
 
          try {
              // Get the image file name
@@ -26,7 +28,6 @@ public class ImageContainerUtil {
              blob.upload(file.getInputStream(),file.getSize(),true);
 
          }catch (Exception e){
-             System.err.println("Errore nel caricamento dell'immagine "+e.getMessage());
              throw new ApiException("Errore nel caricamento dell'immagine", HttpStatus.INTERNAL_SERVER_ERROR);
          }
 
