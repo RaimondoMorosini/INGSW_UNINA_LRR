a6c2bc26389776a7ea88de80cf24d8e0d0be44c0
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-07-09 15:01:54 +0200

    Cleanup: Componet AstaPerRicerca
    Eliminate div In piu' e css che era ridondante

--- SitoVue/ingegneria-project/src/components/vetrinaAndHomepage/AstePerRicerca.vue
+++ SitoVue/ingegneria-project/src/components/vetrinaAndHomepage/AstePerRicerca.vue
@@ -1,74 +1,83 @@
 <template>
     <div class="card p-fluid flex flex-wrap gap-3">
-        <div class="sezione-superiore w-full">
-            <BarraRicercaPerOrdine @ordineSelezionato="setCampiOrdinamento($event)"
-                @prezzoMinSelezionato="setPrezzoMin($event)" @prezzoMaxSelezionato="setPrezzoMax($event)" />
-        </div>
-
-        <div class="sezione-centrale">
-            <!--SEZIONE FILTRI CARETTERISTICHE DEL PRODTTO-->
-            <CaratteristicheProdotto v-if="props.propRicaricaComponenteCaratteristiche"
-                :propCategoria="props.propCategoriaCercata" @caratteristicheSelezionate="setCaratteristiche($event)" />
-            <!--SE CAMBIA CATEGORIA CARICA UN ATTIMO UN TEMPLATE VUOTO PER I FILTRI CARATTERISTICHE IN MODO DA RICARICARE LA COMPONENTE DELLE CARATTERISTICHE QUANDO DIVENTA DI NUOVO TRUE props.propRicaricaComponenteCaratteristiche-->
-            <div v-else></div>
-
-            <!--TAMPLATE CARICAMENTO ASTE-->
-            <CaricamentoAste v-if="props.propLoading" />
-
-            <!--CONTENITORE LISTA ASTE-->
-            <div v-else class="card mx-16 w-auto">
-                <DataView :value="products">
-                    <template #list="slotProps">
-                        <div class="grid-nogutter grid">
-                            <div @click="apriUnNuovoTab(item.idAsta)" v-for="(item, index) in slotProps.items" :key="index" class="col-12 mt-4 contenitore-articolo">
-                                <div class="flex-column sm:align-items-center flex gap-3 bg-zinc-100 p-4 sm:flex-row"
-                                    :class="{ 'border-top-1 surface-border': index !== 0 }">
-                                    <div class="flex-column sm:align-items-center flex gap-3 bg-zinc-100 p-4 sm:flex-row"
-                                        :class="{ 'border-top-1 surface-border': index !== 0 }">
-                                        <div class="md:w-10rem relative">
-                                            <img class="border-round mx-auto block w-full xl:block" :src="`${item.immagini[0]}`" :alt="item.nome" />
-                                            <Tag :value="item.tipoAsta" :severity="danger" class="absolute" style="left: 4px; top: 4px"></Tag>
-                                        </div>
-                                        <div
-                                            class="flex-column justify-content-between md:align-items-center flex flex-1 gap-4 md:flex-row">
-                                            <div
-                                                class="md:flex-column justify-content-between align-items-start flex flex-row gap-2">
-                                                <div>
-                                                    <span class="text-secondary text-sm font-medium">{{ item.categoria
-                                                        }}</span>
-                                                    <div class="text-900 mt-2 text-lg font-medium titolo-articolo">
-                                                        {{ item.nome }}
-                                                    </div>
-                                                    <div>
-                                                        Venditore: {{ item.emailUtenteCreatore }}
-                                                    </div>
-                                                    <span class="text-900 text-xl font-semibold">BASE ASTA: EURO{{item.baseAsta }}</span>
-                                                </div>
-                                            </div>
-                                            <div class="flex-column md:align-items-end flex gap-5">
-                                                <div
-                                                    class="contenitore-bottone flex flex-row-reverse gap-2 bg-primario-100 md:flex-row">
-                                                    <Button @click="apriUnNuovoTab(item.idAsta)"
-                                                        icon="pi pi-shopping-cart" label="Partecipa all'asta" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" 
-                                                        class="white-space-nowrap flex-auto md:flex-initial">
-                                                    </Button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+      <div class="sezione-superiore w-full">
+        <BarraRicercaPerOrdine 
+          @ordineSelezionato="setCampiOrdinamento"
+          @prezzoMinSelezionato="setPrezzoMin"
+          @prezzoMaxSelezionato="setPrezzoMax" 
+        />
+      </div>
+  
+      <div class="sezione-centrale">
+        <CaratteristicheProdotto 
+          v-if="props.propRicaricaComponenteCaratteristiche"
+          :propCategoria="props.propCategoriaCercata" 
+          @caratteristicheSelezionate="setCaratteristiche" 
+        />
+        <div v-else></div>
+  
+        <CaricamentoAste v-if="props.propLoading" />
+  
+        <div v-else class="card mx-16 w-auto">
+          <DataView :value="products">
+            <template #list="slotProps">
+              <div class="grid-nogutter grid">
+                <div 
+                  v-for="(item, index) in slotProps.items" 
+                  :key="index" 
+                  class="col-12 mt-4 contenitore-articolo"
+                  @click="apriUnNuovoTab(item.idAsta)"
+                >
+                  <div 
+                    class="flex-column sm:align-items-center flex gap-3 bg-zinc-100 p-4 sm:flex-row"
+                    :class="{ 'border-top-1 surface-border': index !== 0 }"
+                  >
+                    <div class="md:w-10rem relative">
+                      <img 
+                        class="border-round mx-auto block w-full xl:block" 
+                        :src="`${item.immagini[0]}`" 
+                        :alt="item.nome" 
+                      />
+                      <Tag :value="item.tipoAsta" :severity="'danger'" class="absolute" style="left: 4px; top: 4px"></Tag>
+                    </div>
+                    <div class="flex-column justify-content-between md:align-items-center flex flex-1 gap-4 md:flex-row">
+                      <div class="md:flex-column justify-content-between align-items-start flex flex-row gap-2">
+                        <div>
+                          <span class="text-secondary text-sm font-medium">{{ item.categoria }}</span>
+                          <div class="text-900 mt-2 text-lg font-medium titolo-articolo">{{ item.nome }}</div>
+                          <div>Venditore: {{ item.emailUtenteCreatore }}</div>
+                          <span class="text-900 text-xl font-semibold">BASE ASTA: EURO{{item.baseAsta }}</span>
                         </div>
-                    </template>
-                </DataView>
-            </div>
+                      </div>
+                      <div class="flex-column md:align-items-end flex gap-5">
+                        <div class="contenitore-bottone flex flex-row-reverse gap-2 bg-primario-100 md:flex-row">
+                          <Button 
+                            @click="apriUnNuovoTab(item.idAsta)"
+                            icon="pi pi-shopping-cart" 
+                            label="Partecipa all'asta" 
+                            :disabled="item.inventoryStatus === 'OUTOFSTOCK'" 
+                            class="white-space-nowrap flex-auto md:flex-initial"
+                          >
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </template>
+          </DataView>
         </div>
+      </div>
     </div>
-
-    <!--COMPONENTE DELLE PAGINE-->
-    <Paginator :rows="5" :totalRecords="props.propNumeroAste" @page="onPage($event)"></Paginator>
-</template>
-
+  
+    <Paginator 
+      :rows="5" 
+      :totalRecords="props.propNumeroAste" 
+      @page="onPage"
+    ></Paginator>
+  </template>
+  
 <script setup>
 import DataView from 'primevue/dataview';
 import Tag from 'primevue/tag';
