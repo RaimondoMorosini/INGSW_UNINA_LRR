a764eb5d0ade6495603f0c59c20f3da2bd44fb7f
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-09-25 16:22:07 +0200

    Pulizia codice
    Co-Authored-By: Roberto Spena <105874925+Sperob98@users.noreply.github.com>

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/ProdottoAstaController.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/controller/ProdottoAstaController.java
@@ -1,19 +1,11 @@
 package api.dieti2024.controller;
 
-import api.dieti2024.dto.MultipartfileDTO;
 import api.dieti2024.dto.asta.CreaAstaDTO;
 import api.dieti2024.dto.asta.ImmagineAstaDTO;
-import api.dieti2024.dto.asta.ricerca.FiltroDto;
-import api.dieti2024.dto.asta.ricerca.InfoDatiAstaDTO;
 import api.dieti2024.service.asta.AstaFacadeService;
-import api.dieti2024.service.asta.AstaService;
-import api.dieti2024.service.asta.ProdottoService;
 import api.dieti2024.util.ImageContainerUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 
 @RestController
@@ -22,38 +14,15 @@ import java.util.List;
 public class ProdottoAstaController {
 
     final
-    ProdottoService prodottoService;
-    final
-    AstaService astaService;
-    final
     AstaFacadeService astaFacadeService;
     final
     ImageContainerUtil imageContainerUtil;
 
-    public ProdottoAstaController(ProdottoService prodottoService, AstaService astaService, AstaFacadeService astaFacadeService, ImageContainerUtil imageContainerUtil) {
-        this.prodottoService = prodottoService;
-        this.astaService = astaService;
+    public ProdottoAstaController(AstaFacadeService astaFacadeService, ImageContainerUtil imageContainerUtil) {
         this.astaFacadeService = astaFacadeService;
         this.imageContainerUtil = imageContainerUtil;
     }
 
-    @PostMapping("public/asta/getAllAsteDeprecato")
-    public ResponseEntity< List<InfoDatiAstaDTO>> getAllAste(@RequestBody FiltroDto filtroDto){
-        try {
-            return ResponseEntity.ok( astaService.getAllAstaProdotto(filtroDto));
-        }catch (Exception e){
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    @PostMapping("public/asta/getNumeroAsteDeprecato")
-    public ResponseEntity<Integer> getNumeroTotaleDiAstePerRicerca(@RequestBody FiltroDto filtroDto){
-        try {
-            return ResponseEntity.ok( astaService.getNumeroTotaleDiAstePerRicerca(filtroDto));
-        }catch (Exception e){
-            return ResponseEntity.badRequest().build();
-        }
-    }
 
     @PostMapping("/asta/creaasta")
     public ResponseEntity<String> addProdottoAsta(@RequestBody CreaAstaDTO datiPerCreazioneDtoInput) {
@@ -70,36 +39,6 @@ public class ProdottoAstaController {
         }catch (Exception e){
             return ResponseEntity.badRequest().build();
         }
-    }
-
-    @PostMapping("public/asta/uploadImage")
-    public String uploadImage(@RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return "Errore: Nessun file selezionato";
-        }
-        imageContainerUtil.uploadImage(file,file.getOriginalFilename());
-
-        return "Immagine caricata con successo! ";
-
-
-    }@PostMapping(value = "public/asta/uploadImages" )
-    public String uploadImage(@ModelAttribute MultipartfileDTO input) {
-        List<MultipartFile> files = input.files();
-        System.out.println("id asttttttttttttttttttttaaaaaaaaaaaaaa: "+input.idAsta());
-        if (files.isEmpty()) {
-            return "Errore: Nessun file selezionato";
-        }
-        StringBuilder message= new StringBuilder();
-        for(MultipartFile file:files){
-            if (!file.isEmpty()){
-            imageContainerUtil.uploadImage(file,file.getOriginalFilename());
-            String link = "https://dieti24.blob.core.windows.net/upload/"+file.getOriginalFilename();
-                message.append("Immagine caricata con successo! "+link +"\n");
-            }
-            else message.append("Errore: file non trovato\n");
-        }
-
-        return message.toString();
     }
 
 }
