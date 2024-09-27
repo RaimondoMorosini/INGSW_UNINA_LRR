7620e84b43b6b56cdd16a1e1318d4569a8c2bdd2
Sperob98 <rbrna98@gmail.com>
2024-09-27 18:29:33 +0200

    Creato la galleria del prodotto asta
    creazione della galleria nella pagina dell'asta del prodotto

---
+++ SitoVue/ingegneria-project/src/service/PhotoService.js
@@ -0,0 +1,19 @@
+export const PhotoService = {
+        getData(images) {
+
+            const imageObjects = []; // Array dove aggiungiamo i path delle immagini
+
+            for (let i = 0; i < images.length; i++) {
+                imageObjects.push({
+                    itemImageSrc: images[i],
+                    thumbnailImageSrc: images[i]
+                });
+            }
+
+            return imageObjects
+        },
+
+        getImages(images) {
+            return Promise.resolve(this.getData(images));
+        }
+    };
--- SitoVue/ingegneria-project/src/views/Asta.vue
+++ SitoVue/ingegneria-project/src/views/Asta.vue
@@ -1,27 +1,36 @@
 <template>
-  <br>
-  <div class="asta" v-if="item">
-    <h1>{{ item.nome }} (Categoria: {{ item.categoria }})</h1>
-    <img v-if="item.immagini && item.immagini.length" :src="item.immagini[0]" alt="Immagine Prodotto" class="immagine-prodotto" />
-    <div class="dettagli">
-      <p><strong>Base d'asta:</strong> {{ item.baseAsta }} €</p>
-      <p><strong>Prezzo attuale:</strong> {{ item.prezzoAttuale }} €</p>
-      <p><strong>Tipo d'asta:</strong> {{ item.tipoAsta }}</p>
-      <p><strong>Data di scadenza:</strong> {{ new Date(item.dataScadenza).toLocaleString('it-IT', { timeZone: 'GMT' }) }} GMT</p>
-      <p><strong>Creato da:</strong> {{ item.emailUtenteCreatore }}</p>
-      <button @click="faiOfferta" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Fai un'offerta
-      </button>
-      <Countdown :unixTimestamp="item.dataScadenza" @update:remainingTime="tempoInSecondi = $event"></Countdown>
-      secondi rimanenti: {{ tempoInSecondi }}
+
+  <div class="contenitore_colonne">
+
+    <div class="colonna">
+      <ImmaginiProdotto v-if="item" :prodotto="item" />
     </div>
-    <div class="descrizione">
-      <h2>Descrizione</h2>
-      <p>{{ item.descrizione }}</p>
+    <div class="colonna">
+      <div class="asta" v-if="item">
+        <h1>{{ item.nome }} (Categoria: {{ item.categoria }})</h1>
+        <h1>qui andrà il timer.....</h1>
+        <div class="dettagli">
+          <p><strong>Base d'asta:</strong> {{ item.baseAsta }} €</p>
+          <p><strong>Prezzo attuale:</strong> {{ item.prezzoAttuale }} €</p>
+          <p><strong>Tipo d'asta:</strong> {{ item.tipoAsta }}</p>
+          <p><strong>Data di scadenza:</strong> {{ new Date(item.dataScadenza).toLocaleString('it-IT', { timeZone: 'GMT' }) }} GMT</p>
+          <p><strong>Creato da:</strong> {{ item.emailUtenteCreatore }}</p>
+          <button @click="faiOfferta" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Fai un'offerta
+          </button>
+          <Countdown :unixTimestamp="item.dataScadenza" @update:remainingTime="tempoInSecondi = $event"></Countdown>
+          secondi rimanenti: {{ tempoInSecondi }}
+        </div>
+        <div class="descrizione">
+          <h2>Descrizione</h2>
+          <p>{{ item.descrizione }}</p>
+        </div>
+
+      </div>
+      <div v-else>
+        <p>Caricamento in corso...</p>
+      </div>
     </div>
-  </div>
-  <div v-else>
-    <p>Caricamento in corso...</p>
   </div>
 </template>
 
@@ -31,6 +40,7 @@ import { useRoute } from 'vue-router';
 import { getInfoAstaProdotto } from '../service/PaginaProdottoAstaService';
 import { mantieniAggiornamenti, disconnettiti } from '../scripts/websocket/websocket.js';
 import Countdown from '../components/PaginaAsta/countdown.vue';
+import ImmaginiProdotto from '../components/PaginaAsta/ImmaginiProdotto.vue';
 
 const route = useRoute();
 const astaId = route.params.id;
@@ -90,6 +100,23 @@ function faiOfferta() {
 </script>
 
 <style scoped>
+.contenitore_colonne {
+
+  /* Abilita Flexbox */
+  display: flex;
+  /* Spazia le colonne uniformemente */
+  justify-content: space-between;
+}
+
+.colonna {
+  flex: 1;  /* Ogni colonna occupa spazio uguale */
+  margin: 10px 10px; /* Margine tra le colonne, se necessario */
+  padding: 20px;  /* Padding interno per le colonne */
+  background: linear-gradient(to bottom, #f0f0f0, #ffffff); /* Sfondo grigio chiaro sfumato nel bianco */
+  border-radius: 8px; /* Angoli arrotondati per un aspetto più morbido */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Ombra leggera per profondità */
+}
+
 .asta {
   max-width: 600px;
   margin: 0 auto;
@@ -102,7 +129,8 @@ function faiOfferta() {
   margin: 10px 0;
 }
 
-.dettagli, .descrizione {
+.dettagli,
+.descrizione {
   margin: 20px 0;
 }
 </style>
