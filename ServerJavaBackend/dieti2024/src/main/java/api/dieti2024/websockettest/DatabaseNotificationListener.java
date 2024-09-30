b8c98c77e3f252fa92f78afbb36c273843bed725
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-09-30 17:48:35 +0200

    Update DatabaseNotificationListener.java

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/websockettest/DatabaseNotificationListener.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/websockettest/DatabaseNotificationListener.java
@@ -1,6 +1,5 @@
 package api.dieti2024.websockettest;
 
-
 import jakarta.annotation.PostConstruct;
 import org.postgresql.PGConnection;
 import org.postgresql.PGNotification;
@@ -11,6 +10,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class DatabaseNotificationListener {
@@ -18,33 +18,37 @@ public class DatabaseNotificationListener {
     @Autowired
     private DataSource dataSource;
 
+    private PGConnection pgConnection;
+
     @PostConstruct
-public void listen() {
-    try (Connection conn = dataSource.getConnection()) {
-        PGConnection pgConnection = conn.unwrap(PGConnection.class);
+    public void listen() {
+        try (Connection conn = dataSource.getConnection()) {
+            pgConnection = conn.unwrap(PGConnection.class);
 
-        try (Statement stmt = conn.createStatement()) {
-            stmt.execute("LISTEN offerta_insert_channel");
-        }
-
-        new Thread(() -> {
-            while (true) {
-                try {
-                    PGNotification[] notifications = pgConnection.getNotifications();
-                    if (notifications != null) {
-                        for (PGNotification notification : notifications) {
-                            System.out.println("Received notification: " + notification.getParameter());
-                        }
-                    }
-
-                    Thread.sleep(500);
-                } catch (SQLException | InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute("LISTEN offerta_insert_channel");
             }
-        }).start();
-    } catch (SQLException e) {
-        e.printStackTrace();
+            CompletableFuture.runAsync(this::processNotifications);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-}
+
+    public void processNotifications() {
+        while (true) {
+            try {
+                PGNotification[] notifications = pgConnection.getNotifications();
+                if (notifications != null) {
+                    for (PGNotification notification : notifications) {
+                        System.out.println("Received notification: " + notification.getParameter());
+                    }
+                }
+
+                Thread.sleep(500);
+            } catch (SQLException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
