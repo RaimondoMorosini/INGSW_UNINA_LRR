d557888f000c8359455fb5900fcf2ecf5125695d
Cavaliere <s.lorenzo@studenti.unina.it>
2024-10-02 15:52:48 +0200

    update stepper

--- SitoVue/ingegneria-project/src/views/TestLorenzo.vue
+++ SitoVue/ingegneria-project/src/views/TestLorenzo.vue
@@ -3,26 +3,26 @@
     <div class="card flex justify-center">
         <StepList class="basis-[50rem]">
             <Step value="1">
-                <span class="block sm:hidden">Descrizione Prodotto</span>
-                <span class="hidden sm:block">
+                <span class="hidden lg:block" >Descrizione Prodotto</span>
+                <span class="block lg:hidden">
                     <i class="pi pi-cart-arrow-down"></i>
                 </span>
             </Step>
             <Step value="2">
-                <span class="block sm:hidden">Seleziona Filtri</span>
-                <span class="hidden sm:block">
+                <span class="hidden lg:block">Seleziona Filtri</span>
+                <span class="block lg:hidden">
                     <i class="pi pi-filter"></i>
                 </span>
             </Step>
             <Step value="3">
-                <span class="block sm:hidden">Dettagli Asta</span>
-                <span class="hidden sm:block">
+                <span class="hidden lg:block">Dettagli Asta</span>
+                <span class="block lg:hidden">
                     <i class="pi pi-barcode"></i>
                 </span>
             </Step>
             <Step value="4">
-                <span class="block sm:hidden">Revisione Dati Inseriti</span>
-                <span class="hidden sm:block">
+                <span class="hidden lg:block">Revisione Dati Inseriti</span>
+                <span class="block lg:hidden">
                     <i class="pi pi-check-circle"></i>
                 </span></Step>
         </StepList>
@@ -30,12 +30,26 @@
     <StepPanels>
         <StepPanel v-slot="{ activateCallback }" value="1">
             <CreaProdotto/>
-            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="activateCallback('2')" />
+            <div class="flex justify-around">
+                <Button 
+                raised
+                label="Successivo"
+                size="large"
+                icon="pi pi-arrow-right" 
+                iconPos="right" 
+                @click="datiProdottoInseriti;activateCallback('2')" 
+                class="w-[50%]"
+                >
+                </Button>
+            </div>
         </StepPanel>
         <StepPanel v-slot="{ activateCallback }" value="2">
             <SelezionaFiltri/>
             <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="activateCallback('1')" />
-            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="activateCallback('3')" />
+            <Button 
+                label="Next" 
+                icon="pi pi-arrow-right" 
+                iconPos="right" @click="activateCallback('3')" />
         </StepPanel>
         <StepPanel v-slot="{ activateCallback }" value="3">
             <SelezioneTipoAsta/>
@@ -64,4 +78,19 @@ import StepPanels from 'primevue/steppanels';
 import StepItem from 'primevue/stepitem';
 import Step from 'primevue/step';
 import StepPanel from 'primevue/steppanel';
+
+const datiProdottoInseriti = () => {
+    if (!nomeProdotto.value || !descrizione.value || !prezzoBase.value || !categoriaSalvata) {
+        alert('Compila tutti i campi.');
+        return;
+    }
+
+    storeInstance.updateAsta({
+        nomeProdotto: nomeProdotto.value,
+        descrizione: descrizione.value,
+        prezzoBase: prezzoBase.value,
+        categoria: selectedCategory,
+    });
+    activateCallback('2')
+};
 </script>
