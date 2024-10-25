e3574910004bca9acdea37fb73ab9a0c6908310c
Sperob98 <rbrna98@gmail.com>
2024-10-25 18:46:04 +0200

    Fondamenti pagina logiche

--- SitoVue/ingegneria-project/src/components/Profilo/MessaggiProfilo.vue
+++ SitoVue/ingegneria-project/src/components/Profilo/MessaggiProfilo.vue
@@ -1,5 +1,92 @@
 <template>
-    <div class="flex flex-col gap-3 rounded px-3 py-1">
-        <p class="rounded px-3 ring-1 ring-[#cbd5e1]">Notifiche</p>
+
+    <div class="flex flex-col gap-3 rounded px-2 py-1 mx-auto w-[80%] my-2.5">
+
+        <div class=" flex flex-col">
+            <Dropdown @change="onClickOpzione()" v-model="opzioneSelezionata" :options="opzioniNotifiche"
+                optionLabel="name" placeholder="Ordina per" class="dimensione font-bold" />
+        </div>
+
+        <Card class="h-[300px] overflow-y-scroll">
+            <template #title>
+                {{ titoloOpzione }}
+            </template>
+            <template #content v-if="notifiche">
+                <Accordion @click="onClickNotifica" v-for="(notifica,index) in notifiche">
+                    <AccordionTab :header="notifica.oggettoDellaNotifica">
+                        {{ notifica.messaggio }}
+                    </AccordionTab>
+                </Accordion>
+            </template>
+        </Card>
+
+        <Paginator :rows="5" :totalRecords="numeroNotifiche" @page="onPage"></Paginator>
+
     </div>
+
 </template>
+
+<script setup>
+import { ref,onMounted } from 'vue';
+import Dropdown from 'primevue/dropdown';
+import Card from 'primevue/card';
+import Paginator from 'primevue/paginator';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import { getNotificheNonLette } from '../../service/notifiche';
+
+
+const opzioneSelezionata = ref(null);
+const opzioniNotifiche = ref([
+    { name: 'Notifiche da leggere' },
+    { name: 'Tutte le notifiche ordinati per data (più recenti)' },
+    { name: 'Tutte le notifiche ordinati per data (meno recenti)' },
+]);
+const props = defineProps([
+    'numeroNotificheNonLette',
+]);
+const numeroNotificheNonLette = ref();
+const titoloOpzione = ref('Notifiche da leggere');
+const numeroPagina = ref(0);
+const notifiche = ref();
+
+onMounted(async () => {
+
+    opzioneSelezionata.value = { name: 'Notifiche da leggere' };
+    numeroNotificheNonLette.value = props.numeroNotificheNonLette;
+
+    const parametriRichiestaNotificheNonLette = {
+
+        numeroElementi: 5,
+        numeroPagina: 0
+    };
+
+    if(numeroNotificheNonLette.value > 0) {
+
+        try {
+
+            notifiche.value = await getNotificheNonLette(parametriRichiestaNotificheNonLette);
+
+        } catch (e) {
+
+            console.error("get notifiche fallitoooooo:", e);
+        }
+    }
+});
+
+const onClickOpzione = () => {
+
+    titoloOpzione.value = opzioneSelezionata.value.name;
+};
+
+const onPage = (event) => {
+
+    console.log(event.page + 1);
+};
+
+const onClickNotifica = () => {
+    
+    numeroNotificheNonLette.value--;
+};
+
+</script>
--- SitoVue/ingegneria-project/src/views/ProfiloView.vue
+++ SitoVue/ingegneria-project/src/views/ProfiloView.vue
@@ -1,49 +1,72 @@
 <template>
-    <ul class="flex flex-row justify-between border-b-2 border-slate-300 bg-slate-200/20 px-3 py-2">
-        <li
-            @click="pagina = 1"
-            class="flex rounded px-2 py-1 font-bold hover:bg-primario-400 hover:  hover:ring-2 hover:ring-slate-200/50"
-        >
+    <ul class="flex flex-row justify-between border-b-2 border-slate-300 bg-slate-200/20 px-3 py-2 cursor-pointer">
+        <li @click="pagina = 1"
+            class="flex rounded px-2 py-1 font-bold hover:bg-primario-400 hover:  hover:ring-2 hover:ring-slate-200/50">
             <span>
                 <i class="pi pi-user" style="font-size: 1.5rem; padding-right: 0.8rem"></i>
             </span>
             Informazioni Profilo
         </li>
-        <li
-            @click="pagina = 2"
-            class="flex rounded px-2 py-1 font-bold hover:bg-primario-400 hover:  hover:ring-2 hover:ring-slate-200/50"
-        >
+        <li @click="pagina = 2"
+            class="flex rounded px-2 py-1 font-bold hover:bg-primario-400 hover:  hover:ring-2 hover:ring-slate-200/50 cursor-pointer">
             <span>
                 <i class="pi pi-inbox" style="font-size: 1.5rem; padding-right: 0.8rem"></i>
             </span>
             Notifiche
-            <Badge v-if="checked"></Badge>
+            <Badge v-if="checked">{{ numeroNotificheDaLeggere }}</Badge>
         </li>
-        <li
-            @click="pagina = 3"
-            class="flex rounded px-2 py-1 font-bold hover:bg-primario-400 hover:  hover:ring-2 hover:ring-slate-200/50"
-        >
+        <li @click="pagina = 3"
+            class="flex rounded px-2 py-1 font-bold hover:bg-primario-400 hover:  hover:ring-2 hover:ring-slate-200/50 cursor-pointer">
             <span>
                 <i class="pi pi-shopping-bag" style="font-size: 1.5rem; padding-right: 0.8rem"></i>
             </span>
             Aste Personali
         </li>
     </ul>
-    <div v-if="pagina === 1"><InfoProfilo /></div>
-    <div v-else-if="pagina === 2"><Notifiche /></div>
-    <div v-else-if="pagina === 3"><AstePersonali /></div>
+    <div v-if="pagina === 1">
+        <InfoProfilo />
+    </div>
+    <div v-else-if="pagina === 2">
+        <Notifiche v-if="numeroNotificheDaLeggere" :numeroNotificheNonLette="numeroNotificheDaLeggere" @emitNotifica="clickNotifica($event)"/>
+    </div>
+    <div v-else-if="pagina === 3">
+        <AstePersonali />
+    </div>
 </template>
 
 <script setup>
 import { useToast } from 'primevue/usetoast';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import InfoProfilo from '../components/Profilo/InformazioniProfilo.vue';
 import Notifiche from '../components/Profilo/MessaggiProfilo.vue';
 import AstePersonali from '../components/Profilo/AstePersonaliProfilo.vue';
 import Badge from 'primevue/badge';
+import { getNumeroDiNotificheNonLette } from '../service/notifiche';
 
-const checked = ref(false);
-
+const checked = ref(true);
 const pagina = ref(1);
 const toast = useToast();
+const numeroNotificheDaLeggere = ref();
+
+onMounted(async () => {
+
+    try {
+
+        numeroNotificheDaLeggere.value = await getNumeroDiNotificheNonLette();
+        if (numeroNotificheDaLeggere.value > 0) {
+            checked.value = true;
+        } else {
+            checked.value = false;
+        }
+
+    } catch (e) {
+        console.error("Errore richiesta numero notificheeeee:", e);
+    }
+});
+
+clickNotifica = (numeroNotificheNonLette) => {
+
+    numeroNotificheDaLeggere.value = numeroNotificheNonLette;
+};
+
 </script>
