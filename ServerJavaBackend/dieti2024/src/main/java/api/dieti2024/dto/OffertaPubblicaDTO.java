e370428640d85e52b2dc6154e933ce3b1198d5f7
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2024-11-27 15:48:34 +0100

    cambiato nome alla dto per maggior chiarezza

--- ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/dto/OffertaPubblicaDTO.java
+++ ServerJavaBackend/dieti2024/src/main/java/api/dieti2024/dto/OffertaPubblicaDTO.java
@@ -1,12 +1,12 @@
 package api.dieti2024.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @Builder
-public class OffertaAstaIngleseDTO {
+public class OffertaPubblicaDTO {
+
 
     String emailUtente;
 
@@ -14,7 +14,7 @@ public class OffertaAstaIngleseDTO {
 
     long tempoOfferta;
 
-    public OffertaAstaIngleseDTO(String utente, double prezzoOfferto, long tempo) {
+    public OffertaPubblicaDTO(String utente, double prezzoOfferto, long tempo) {
         this.emailUtente = utente;
         this.prezzoProposto = prezzoOfferto;
         this.tempoOfferta = tempo;
