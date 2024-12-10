54c8377c16ebc0d4a6315f4d763e95568558d89c
Sperob98 <rbrna98@gmail.com>
2024-12-11 00:58:03 +0100

    Aggiunto responsivitò al design delle caratteristiche dei prodotti nella ricerca

--- SitoVue/ingegneria-project/src/components/vetrinaAndHomepage/AstePerRicerca.vue
+++ SitoVue/ingegneria-project/src/components/vetrinaAndHomepage/AstePerRicerca.vue
@@ -1,20 +1,41 @@
 <template>
     <div class="card justify-between gap-3">
         <div class="sezione-superiore w-full">
-            <barraDeiFIltriPrincipale
-                @ordineSelezionato="setCampiOrdinamento"
-                @prezzoMinSelezionato="setPrezzoMin"
-                @prezzoMaxSelezionato="setPrezzoMax"
-            />
+            <barraDeiFIltriPrincipale @ordineSelezionato="setCampiOrdinamento" @prezzoMinSelezionato="setPrezzoMin"
+                @prezzoMaxSelezionato="setPrezzoMax" />
         </div>
 
+        <div class="flex h-auto flex-col lg:flex-row justify-center rounded bg-slate-200/20 p-2 m-4 
+            lg:mr-4 mb-4 lg:mb-0 lg:space-x-4 block md:hidden">
+
+            <Button label="Filtra caratteritiche" @click="visible = true" />
+         </div>
+
         <div class="sezione-centrale">
-            <CaratteristicheProdotto
-                v-if="props.propRicaricaComponenteCaratteristiche"
+
+            <div class="block md:hidden card flex justify-center">
+
+                <Dialog v-model:visible="visible" modal header="Filtri" :style="{ width: '50vw' }"
+                    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+
+                    <div class="h-[500px] flex flex-col overflow-y-auto">
+                        <CaratteristicheProdotto class="m-o" v-if="props.propRicaricaComponenteCaratteristiche"
+                            :propCategoria="props.propCategoriaCercata"
+                            :propCaratteristicheselezionate="props.propCaratteristicheselezionate"
+                            @caratteristicheSelezionate="setCaratteristiche" />
+                    </div>
+
+                    <div>
+                        <Button label="Mostra risultati" class="w-full" @click="clickFiltro" />
+                    </div>
+
+                </Dialog>
+            </div>
+
+            <CaratteristicheProdotto class="hidden md:block" v-if="props.propRicaricaComponenteCaratteristiche"
                 :propCategoria="props.propCategoriaCercata"
                 :propCaratteristicheselezionate="props.propCaratteristicheselezionate"
-                @caratteristicheSelezionate="setCaratteristiche"
-            />
+                @caratteristicheSelezionate="setCaratteristiche" />
             <div v-else></div>
 
             <CaricamentoAste v-if="props.propLoading" />
@@ -23,13 +44,8 @@
                 <DataView :value="products">
                     <template #list="slotProps">
                         <div class="grid-nogutter grid">
-                            <ProductItem
-                                v-for="(item, index) in slotProps.items"
-                                :key="index"
-                                :item="item"
-                                :index="index"
-                                class="col-12 contenitore-articolo mt-4"
-                            ></ProductItem>
+                            <ProductItem v-for="(item, index) in slotProps.items" :key="index" :item="item"
+                                :index="index" class="col-12 contenitore-articolo mt-4"></ProductItem>
                         </div>
                     </template>
                 </DataView>
@@ -51,6 +67,12 @@ import CaricamentoAste from './CaricamentoAste.vue';
 import CaratteristicheProdotto from './caratteristicaProdotti/CaratteristicheProdotto.vue';
 import barraDeiFIltriPrincipale from './barraDeiFIltriPrincipale.vue';
 import ProductItem from './singolo-prodotto/ProductItem.vue';
+
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+
+const visible = ref(false);
+
 const emit = defineEmits([
     'caratteristicheSelezionate',
     'numeroDiPaginaSelezionato',
@@ -66,10 +88,18 @@ const props = defineProps([
     'propCategoriaCercata',
     'propRicaricaComponenteCaratteristiche',
     'propCaratteristicheselezionate',
+    'propMostraRisultati',
 ]);
 
 const products = ref();
 products.value = props.propAste;
+
+const clickFiltro = () => {
+
+    visible.value = false;
+    props.propMostraRisultati();
+    console.log('clickFiltro');
+};
 
 watch(
     () => props.propAste,
