145b46a215cabe6499b6b0656a296acc11e61bc7
Sperob98 <rbrna98@gmail.com>
2024-12-11 15:10:06 +0100

    link asta nelle notifiche

--- SitoVue/ingegneria-project/src/components/Profilo/ListaNotifiche/NotificaItem/NotificationMessage.vue
+++ SitoVue/ingegneria-project/src/components/Profilo/ListaNotifiche/NotificaItem/NotificationMessage.vue
@@ -4,18 +4,31 @@
     </p>
     <p v-if="isMessageVisible" class="notification-message-full">
         {{ message }}
-        <strong>Asta ID:</strong> {{ astaId }}
+            <strong class="text-blue-500 underline cursor-pointer" @click="clickVaiAllAsta(astaId)">Vai all'asta:</strong>
     </p>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter(); // Usa il router
 
 const props = defineProps({
     message: String,
     isMessageVisible: Boolean,
     toggleMessage: Function,
+    astaId: Number,
 });
+
+const clickVaiAllAsta = (idAsta) => {
+
+router.push({
+
+    path: '/asta/'+idAsta,
+})
+}
+
 </script>
 
 <style scoped>
@@ -24,13 +37,16 @@ const props = defineProps({
     color: #555;
     margin-top: 5px;
     display: -webkit-box;
-    -webkit-line-clamp: 2; /* Limita a due righe visibili */
+    -webkit-line-clamp: 2;
+    /* Limita a due righe visibili */
     -webkit-box-orient: vertical;
     overflow: hidden;
-    text-overflow: ellipsis; /* Mostra i puntini di sospensione per il testo troncato */
+    text-overflow: ellipsis;
+    /* Mostra i puntini di sospensione per il testo troncato */
     cursor: pointer;
     transition: max-height 0.3s ease;
 }
+
 .notification-message-full {
     font-size: 0.95em;
     color: #333;
